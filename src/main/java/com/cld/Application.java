69e059f3cc92e47e2de76579244010175cb3ff01
com_dong <dongdingming@sina.cn>
2017-04-13 17:32:42 +0800

    modify userController

---
+++ src/main/java/com/cld/Application.java
@@ -0,0 +1,16 @@
+package com.cld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dong on 2017/4/13.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+
+}
--- src/main/java/com/cld/controller/UserController.java
+++ src/main/java/com/cld/controller/UserController.java
@@ -1,7 +1,17 @@
 package com.cld.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by dong on 2017/4/13.
  */
+@RestController
 public class UserController {
+
+    @RequestMapping(value = "hello")
+    public String hello(){
+        return "hello";
+    }
+
 }
