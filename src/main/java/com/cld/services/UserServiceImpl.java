ea5cb4b8e23c51e937dc245df79119d6d0698dd8
com_dong <dongdingming@sina.cn>
2017-04-14 14:12:28 +0800

    modify userController

---
+++ src/main/java/com/cld/services/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.cld.services;
+
+import com.cld.bean.User;
+import com.cld.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by dong on 2017/4/13.
+ */
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public List<User> findAll() {
+        return userMapper.findAll();
+    }
+}
