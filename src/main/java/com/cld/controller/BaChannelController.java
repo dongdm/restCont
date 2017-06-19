bb5647e4c90c09a384a19464e07ed3045520963d
sunzb <2052584477@qq.com>
2017-06-19 09:31:01 +0800

    修改

--- src/main/java/com/cld/controller/BaChannelController.java
+++ src/main/java/com/cld/controller/BaChannelController.java
@@ -1,7 +1,5 @@
 package com.cld.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
 import com.cld.bean.BaChannel;
 import com.cld.services.BaChannelService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,20 +26,6 @@ public class BaChannelController {
 
     @RequestMapping("/tree")
     public String tree(Model modeal){
-        BaChannel baChannel = new BaChannel();
-        baChannel.setType("book");
-        List<BaChannel> list = baChannelService.searchAll(baChannel);
-        ArrayList<Map<String,Object>> stringList = new ArrayList<Map<String, Object>>();
-        for (int i = 0 ;i<list.size();i++){
-            Map<String,Object> map = new HashMap<String,Object>();
-            map.put("id",list.get(i).getId());
-            map.put("pId",list.get(i).getParentid());
-            map.put("name",list.get(i).getChannelname());
-            map.put("link",list.get(i).getUrl());
-            map.put("mark",list.get(i).getDescription()==null?"":list.get(i).getDescription());
-            stringList.add(map);
-        }
-        modeal.addAttribute("list", JSONArray.toJSONStringWithDateFormat(stringList, JSON.VERSION));
         return"/baseTree";
     }
 
@@ -53,6 +39,26 @@ public class BaChannelController {
     public String mergeTree( @ModelAttribute BaChannel baChannel){
         baChannelService.merge(baChannel);
         return "redirect:/tree";
+    }
+
+
+    @RequestMapping(value="/treeList",method = RequestMethod.POST)
+    @ResponseBody
+    public  ArrayList<Map<String,Object>> treeList(){
+        BaChannel baChannel = new BaChannel();
+        baChannel.setType("book");
+        List<BaChannel> list = baChannelService.searchAll(baChannel);
+        ArrayList<Map<String,Object>> stringList = new ArrayList<Map<String, Object>>();
+        for (int i = 0 ;i<list.size();i++){
+            Map<String,Object> map = new HashMap<String,Object>();
+            map.put("id",list.get(i).getId());
+            map.put("pId",list.get(i).getParentid());
+            map.put("name",list.get(i).getChannelname());
+            map.put("link",list.get(i).getUrl());
+            map.put("mark",list.get(i).getDescription()==null?"":list.get(i).getDescription());
+            stringList.add(map);
+        }
+        return stringList;
     }
 
 }
